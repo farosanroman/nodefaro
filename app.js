82d41f29d181eeb627fe633d2d518287e652370c
farosanroman <ppazpurua@gmail.com>
2018-10-15 14:03:20 -0400

    ppa

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ var sms = require('./shared/sms.js');
 //oJo Application Settings WEBSITE_NODE_DEFAULT_VERSION 6.9.1  oJo oJo
 
 app.use(express.json())
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.get('/',(req,res)=>{
   res.send("NodeFaro v0.0.0")
   //var ppa=queries.f();
